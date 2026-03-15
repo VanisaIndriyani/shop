c37d1863692abba3257112d22a25bb2fd5b4384b
VanisaIndriyani <vanisa@example.com>
2026-03-15 23:31:06 +0700

    ui: make navbar fully transparent

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -123,7 +123,7 @@
         }
     }"
     x-init="init()"
-    class="bg-white sticky top-0 z-50 border-b border-gray-200/60">
+    class="bg-transparent fixed top-0 inset-x-0 z-50">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16 items-center">
             <!-- Left: Hamburger Menu & Logo -->
